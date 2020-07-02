861e0c326021f5af78a25f2c91470545a2bf0666
AdamTeodoro <sradamteodoro@gmail.com>
2020-07-02 17:06:03 -0300

    finalizando graficos

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -11,26 +11,49 @@ export class AboutComponent implements OnInit {
     public mostra: boolean = false;
     constructor() { }
 
-    public pieChartOptions: ChartOptions = {
+    public pieChartOptionsJobs: ChartOptions = {
         responsive: true,
         legend: {
             labels: {
                 fontSize: 20
             }
-        }
+        },
+
     };
 
-    public pieChartLabels: Label[] = [ 'Manutenção', 'Aplicações inteiras', 'Melhoria e performance'];
-    public pieChartData: SingleDataSet = [31, 18, 51];
-    public pieChartcolors: Color[] = [
+    public pieChartLabelsJobs: Label[] = [ 'Manutenção', 'Aplicações inteiras', 'Melhoria e performance'];
+    public pieChartDataJobs: SingleDataSet = [154, 54, 220];
+    public pieChartcolorsJobs: Color[] = [
         {
-            backgroundColor: ["#333", "rgb(226, 11, 47)", "#ddd"]
+            backgroundColor: ["#333", "#50d5e1", "#ddd"]
         }
     ];
-    public pieChartType: ChartType = 'pie';
-    public pieChartLegend = true;
-    public pieChartPlugins = [];
+    public pieChartTypeJobs: ChartType = 'pie';
+    public pieChartLegendJobs = true;
+    public pieChartPluginsJobs = [];
 
     ngOnInit(): void {
     }
+
+    public pieChartOptionsInfo: ChartOptions = {
+        responsive: true,
+        legend: {
+            labels: {
+                fontSize: 20
+            }
+        },
+
+    };
+
+    public pieChartLabelsInfo: Label[] = [ 'Abandonados e mortos', 'Em produção', 'Em desenvolvimento'];
+    public pieChartDataInfo: SingleDataSet = [48, 380, 16];
+    public pieChartcolorsInfo: Color[] = [
+        {
+            backgroundColor: ["#333", "#ddd", "rgb(226, 11, 47)"]
+        }
+    ];
+    public pieChartTypeInfo: ChartType = 'pie';
+    public pieChartLegendInfo = true;
+    public pieChartPluginsInfo = [];
+
 }
